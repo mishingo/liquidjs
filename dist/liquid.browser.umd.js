96776cbba8d02afebfe5d65031bdf5d093117d13
mishingo <mr@mishingo.com>
2024-08-13 22:24:43 -0400

    t1

--- dist/liquid.browser.umd.js
+++ dist/liquid.browser.umd.js
@@ -1584,14 +1584,8 @@
                     // Handle `${...}` syntax
                     console.log('token', token, typeof token);
                     if (!('content' in token && typeof token.content === 'string')) return [3 /*break*/, 3];
-                    console.log('passed check');
-                    console.log(token.content);
-                    console.log(token.content.startsWith('${'));
-                    console.log(token.content.endsWith('}'));
                     if (!(token.content.startsWith('${') && token.content.endsWith('}'))) return [3 /*break*/, 2];
-                    console.log('passed check 2');
                     variableName = token.content.slice(2, -1).trim();
-                    console.log(ctx._get(variableName.split('.')));
                     return [4 /*yield*/, ctx._get(variableName.split('.'))];
                 case 1: return [2 /*return*/, _a.sent()];
                 case 2: return [2 /*return*/, token.content];
--- src/render/expression.ts
+++ src/render/expression.ts
@@ -39,14 +39,8 @@ export function * evalToken (token: Token | undefined, ctx: Context, lenient = f
   // Handle `${...}` syntax
   console.log('token', token, typeof token)
   if ('content' in token && typeof token.content === 'string') {
-    console.log('passed check')
-    console.log(token.content)
-    console.log(token.content.startsWith('${'))
-    console.log(token.content.endsWith('}'))
     if (token.content.startsWith('${') && token.content.endsWith('}')) {
-      console.log('passed check 2')
       const variableName = token.content.slice(2, -1).trim()
-      console.log(ctx._get(variableName.split('.')))
       return yield ctx._get(variableName.split('.'))
     }
     return token.content
