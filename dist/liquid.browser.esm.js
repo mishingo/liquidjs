c6b8b374150208c174e214d775ad4e637563f3ee
mishingo <mr@mishingo.com>
2024-08-14 21:36:32 -0400

    check

--- dist/liquid.browser.esm.js
+++ dist/liquid.browser.esm.js
@@ -1953,7 +1953,7 @@ class Tokenizer {
             return new FilteredValueToken(dynamicExpression, [], this.input, begin, this.p, this.file);
         }
         const initial = this.readExpression();
-        this.assert(initial.valid(), `invalid value expression: ${this.snapshot()}`);
+        this.assert(initial.valid(), `invalid value expression: ${this.snapshot()} : ${initial}`);
         const filters = this.readFilters();
         return new FilteredValueToken(initial, filters, this.input, begin, this.p, this.file);
     }
