1caf1659d12e43043ab3042a2e150058685b03b7
mishingo <mr@mishingo.com>
2024-08-19 17:44:09 -0400

    front run the dollar curly

--- dist/liquid.node.cjs.js
+++ dist/liquid.node.cjs.js
@@ -2701,8 +2701,8 @@ class Parser {
         this.parseLimit = new Limiter('parse length', liquid.options.parseLimit);
     }
     parse(html, filepath) {
-        html = String(html);
-        console.log(html.replace(/\$\{([^}]+)\}/g, '$1'));
+        html = String(html.replace(/\$\{([^}]+)\}/g, '$1'));
+        console.log(html);
         this.parseLimit.use(html.length);
         const tokenizer = new Tokenizer(html, this.liquid.options.operators, filepath);
         const tokens = tokenizer.readTopLevelTokens(this.liquid.options);
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -26,8 +26,8 @@ export class Parser {
     this.parseLimit = new Limiter('parse length', liquid.options.parseLimit)
   }
   public parse (html: string, filepath?: string): Template[] {
-    html = String(html)
-    console.log(html.replace(/\$\{([^}]+)\}/g, '$1'))
+    html = String(html.replace(/\$\{([^}]+)\}/g, '$1'))
+    console.log(html)
     this.parseLimit.use(html.length)
     const tokenizer = new Tokenizer(html, this.liquid.options.operators, filepath)
     const tokens = tokenizer.readTopLevelTokens(this.liquid.options)
